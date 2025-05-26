3d5d6c2263f3b00142486f3297f91743e5160201
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-05-25 23:52:36 -0300

    feat: persist each data to cache between calls

--- Commands/TranslateCommand.cs
+++ Commands/TranslateCommand.cs
@@ -7,25 +7,47 @@ namespace ApoloDictionary.Commands
     public class TranslateCommandFactory : CommandFactory
     {
         private Option<string> _textOption;
-        public TranslateCommandFactory() : base("translate", "Translate a word or phrase") { }
+        private CacheManager _cacheManager;
+        public TranslateCommandFactory() : base("translate", "Translate a word or phrase") {
+            _cacheManager = CacheManager.Instance;
+        }
         protected override void AddOptions(Command command)
         {
             _textOption = new Option<string>("--text", "The text to translate") { IsRequired = true };
             command.AddOption(_textOption);
         }
+        private IEnumerable<WordDefinition>? GetFromCache(string textValue)
+        {
+            return _cacheManager.GetFromCache(textValue);
+        }
+        private void SaveToCache(string textValue, IEnumerable<WordDefinition> definitions)
+        {
+            _cacheManager.SaveToCache(textValue, definitions);
+        }
+        private void show(IEnumerable<WordDefinition> definitions)
+        {
+            foreach (WordDefinition de in definitions)
+            {
+                Console.WriteLine(de);
+                Console.WriteLine("--------------");
+            }
+        }
         protected override void DefineHandler(Command command)
         {
             command.SetHandler((textValue) =>
             {
+                IEnumerable<WordDefinition>? cached = GetFromCache(textValue);
+                if(cached != null && cached.Count() > 0)
+                {
+                    show(cached);
+                    return;
+                }
                 ITranslatorProvider provider = new CambridgeDictionaryProvider();
                 try
                 {
                     IEnumerable<WordDefinition> definitions = provider.Translate(textValue);
-                    foreach(WordDefinition de in definitions)
-                    {
-                        Console.WriteLine(de);
-                        Console.WriteLine("--------------");
-                    }                    
+                    show(definitions);
+                    SaveToCache(textValue, definitions);
                 } catch(Exception ex)
                 {
                     Console.WriteLine($"Provider {provider} could not translate properly {textValue}.");
---
+++ Domain/CacheManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ApoloDictionary.Domain
+{
+    public class CacheManager
+    {
+        private string _cacheFilePath;
+        private static CacheManager? _instance;
+        public static CacheManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new CacheManager();
+                }
+                return _instance;
+            }
+        }
+        private CacheManager()
+        {
+            _cacheFilePath = Path.GetFullPath("./cache");
+            LoadCache();
+        }
+        private void LoadCache()
+        {
+            if (!Directory.Exists(_cacheFilePath))
+            {
+                Directory.CreateDirectory(_cacheFilePath);
+            }
+        }
+        public IEnumerable<WordDefinition>? GetFromCache(string key)
+        {
+            string pathToFile = Path.Combine(_cacheFilePath, $"{WebUtility.UrlEncode(key)}.json");
+            if(!File.Exists(pathToFile)) return null;
+            List<WordDefinition>? wordDefinitions = JsonSerializer.Deserialize<List<WordDefinition>>(File.ReadAllText(pathToFile));
+            if(wordDefinitions == null || wordDefinitions.Count == 0) return null;
+            return wordDefinitions.AsEnumerable();
+        }
+        public void SaveToCache(string key, IEnumerable<WordDefinition> definition)
+        {
+            List<WordDefinition> definitionsList = definition.ToList();
+            string pathToFile = Path.Combine(_cacheFilePath, $"{WebUtility.UrlEncode(key)}.json");
+            string json = JsonSerializer.Serialize(definitionsList);
+            File.WriteAllText(pathToFile, json);
+        }
+    }
+}
